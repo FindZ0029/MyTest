3ee4bbdeb2a38fe2e0f74b887c24f84387401e1e
MCKENZIE <findel01@gmail.com>
2016-09-15 18:17:06 +0800

    Update

--- WHATechChallenge/WHATechChallenge/Form1.Designer.cs
+++ WHATechChallenge/WHATechChallenge/Form1.Designer.cs
@@ -39,10 +39,18 @@
             this.dgvSettled = new System.Windows.Forms.DataGridView();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
             this.dgvUnsettled = new System.Windows.Forms.DataGridView();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.dgvSettledSum = new System.Windows.Forms.DataGridView();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.dgvUnsettledSum = new System.Windows.Forms.DataGridView();
             this.groupBox1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dgvSettled)).BeginInit();
             this.groupBox2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dgvUnsettled)).BeginInit();
+            this.groupBox3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvSettledSum)).BeginInit();
+            this.groupBox4.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvUnsettledSum)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
@@ -65,44 +73,49 @@
             // 
             // txtSettled
             // 
+            this.txtSettled.Enabled = false;
             this.txtSettled.Location = new System.Drawing.Point(141, 19);
             this.txtSettled.Name = "txtSettled";
-            this.txtSettled.Size = new System.Drawing.Size(208, 20);
+            this.txtSettled.Size = new System.Drawing.Size(373, 20);
             this.txtSettled.TabIndex = 1;
             // 
             // txtUnsettled
             // 
+            this.txtUnsettled.Enabled = false;
             this.txtUnsettled.Location = new System.Drawing.Point(141, 46);
             this.txtUnsettled.Name = "txtUnsettled";
-            this.txtUnsettled.Size = new System.Drawing.Size(208, 20);
+            this.txtUnsettled.Size = new System.Drawing.Size(373, 20);
             this.txtUnsettled.TabIndex = 1;
             // 
             // btnBrowse1
             // 
-            this.btnBrowse1.Location = new System.Drawing.Point(355, 19);
+            this.btnBrowse1.Location = new System.Drawing.Point(520, 19);
             this.btnBrowse1.Name = "btnBrowse1";
             this.btnBrowse1.Size = new System.Drawing.Size(24, 20);
             this.btnBrowse1.TabIndex = 2;
             this.btnBrowse1.Text = "...";
             this.btnBrowse1.UseVisualStyleBackColor = true;
+            this.btnBrowse1.Click += new System.EventHandler(this.btnBrowse1_Click);
             // 
             // btnBrowse2
             // 
-            this.btnBrowse2.Location = new System.Drawing.Point(355, 46);
+            this.btnBrowse2.Location = new System.Drawing.Point(520, 46);
             this.btnBrowse2.Name = "btnBrowse2";
             this.btnBrowse2.Size = new System.Drawing.Size(24, 20);
             this.btnBrowse2.TabIndex = 2;
             this.btnBrowse2.Text = "...";
             this.btnBrowse2.UseVisualStyleBackColor = true;
+            this.btnBrowse2.Click += new System.EventHandler(this.btnBrowse2_Click);
             // 
             // btnEvaluate
             // 
-            this.btnEvaluate.Location = new System.Drawing.Point(385, 19);
+            this.btnEvaluate.Location = new System.Drawing.Point(550, 19);
             this.btnEvaluate.Name = "btnEvaluate";
             this.btnEvaluate.Size = new System.Drawing.Size(91, 47);
             this.btnEvaluate.TabIndex = 3;
             this.btnEvaluate.Text = "Evaluate";
             this.btnEvaluate.UseVisualStyleBackColor = true;
+            this.btnEvaluate.Click += new System.EventHandler(this.btnEvaluate_Click);
             // 
             // groupBox1
             // 
@@ -116,10 +129,15 @@
             // 
             // dgvSettled
             // 
+            this.dgvSettled.AllowUserToAddRows = false;
+            this.dgvSettled.AllowUserToDeleteRows = false;
+            this.dgvSettled.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
             this.dgvSettled.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dgvSettled.Dock = System.Windows.Forms.DockStyle.Fill;
             this.dgvSettled.Location = new System.Drawing.Point(3, 16);
             this.dgvSettled.Name = "dgvSettled";
+            this.dgvSettled.ReadOnly = true;
+            this.dgvSettled.RowHeadersVisible = false;
             this.dgvSettled.Size = new System.Drawing.Size(455, 145);
             this.dgvSettled.TabIndex = 0;
             // 
@@ -135,19 +153,74 @@
             // 
             // dgvUnsettled
             // 
+            this.dgvUnsettled.AllowUserToAddRows = false;
+            this.dgvUnsettled.AllowUserToDeleteRows = false;
+            this.dgvUnsettled.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
             this.dgvUnsettled.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dgvUnsettled.Dock = System.Windows.Forms.DockStyle.Fill;
             this.dgvUnsettled.Location = new System.Drawing.Point(3, 16);
             this.dgvUnsettled.Name = "dgvUnsettled";
+            this.dgvUnsettled.ReadOnly = true;
+            this.dgvUnsettled.RowHeadersVisible = false;
             this.dgvUnsettled.Size = new System.Drawing.Size(455, 145);
             this.dgvUnsettled.TabIndex = 0;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.dgvSettledSum);
+            this.groupBox3.Location = new System.Drawing.Point(482, 76);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(159, 164);
+            this.groupBox3.TabIndex = 4;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Summary";
+            // 
+            // dgvSettledSum
+            // 
+            this.dgvSettledSum.AllowUserToAddRows = false;
+            this.dgvSettledSum.AllowUserToDeleteRows = false;
+            this.dgvSettledSum.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvSettledSum.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvSettledSum.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgvSettledSum.Location = new System.Drawing.Point(3, 16);
+            this.dgvSettledSum.Name = "dgvSettledSum";
+            this.dgvSettledSum.ReadOnly = true;
+            this.dgvSettledSum.RowHeadersVisible = false;
+            this.dgvSettledSum.Size = new System.Drawing.Size(153, 145);
+            this.dgvSettledSum.TabIndex = 0;
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.dgvUnsettledSum);
+            this.groupBox4.Location = new System.Drawing.Point(482, 246);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(159, 164);
+            this.groupBox4.TabIndex = 4;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Summary";
+            // 
+            // dgvUnsettledSum
+            // 
+            this.dgvUnsettledSum.AllowUserToAddRows = false;
+            this.dgvUnsettledSum.AllowUserToDeleteRows = false;
+            this.dgvUnsettledSum.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvUnsettledSum.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvUnsettledSum.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgvUnsettledSum.Location = new System.Drawing.Point(3, 16);
+            this.dgvUnsettledSum.Name = "dgvUnsettledSum";
+            this.dgvUnsettledSum.ReadOnly = true;
+            this.dgvUnsettledSum.RowHeadersVisible = false;
+            this.dgvUnsettledSum.Size = new System.Drawing.Size(153, 145);
+            this.dgvUnsettledSum.TabIndex = 0;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(488, 425);
+            this.ClientSize = new System.Drawing.Size(653, 425);
             this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.groupBox3);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.btnEvaluate);
             this.Controls.Add(this.btnBrowse2);
@@ -164,6 +237,10 @@
             ((System.ComponentModel.ISupportInitialize)(this.dgvSettled)).EndInit();
             this.groupBox2.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.dgvUnsettled)).EndInit();
+            this.groupBox3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvSettledSum)).EndInit();
+            this.groupBox4.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvUnsettledSum)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -182,6 +259,10 @@
         private System.Windows.Forms.DataGridView dgvSettled;
         private System.Windows.Forms.GroupBox groupBox2;
         private System.Windows.Forms.DataGridView dgvUnsettled;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.DataGridView dgvSettledSum;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.DataGridView dgvUnsettledSum;
     }
 }
 
--- WHATechChallenge/WHATechChallenge/Form1.cs
+++ WHATechChallenge/WHATechChallenge/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +13,118 @@ namespace WHATechChallenge
 {
     public partial class Form1 : Form
     {
+        public DataTable TableSettled = new DataTable();
+        public DataTable TableUnsettled = new DataTable();
+
+        public DataTable TableSettledSum = new DataTable();
+        public DataTable TableUnsettledSum = new DataTable();
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        public string GetFileLoc()
+        {
+            OpenFileDialog _fileLoc = new OpenFileDialog();
+
+            _fileLoc.ShowDialog();
+
+            return _fileLoc.FileName;
+        }
+
+
+        public DataTable FillTable(DataTable DT, string Path)
+        {
+            int x, y;
+            string[] alllines = File.ReadAllLines(Path);
+
+            for (x = 0; x <= alllines.Count() - 1; x++)
+            {
+                if (x != 0)
+                {
+                    string[] MyRow = alllines[x].Split(',');
+                    DT.Rows.Add();
+
+                    for (y = 0; y <= DT.Columns.Count - 1; y++)
+                    {
+                        DT.Rows[x - 1][y] = MyRow[y];
+                    }
+                }
+                else
+                {
+                    string[] Myheaders = alllines[x].Split(',');
+                    foreach (string header in Myheaders)
+                    {
+                        DT.Columns.Add(header);
+                    }
+                }
+            }
+
+            return DT;
+        }
+
+        private void btnBrowse1_Click(object sender, EventArgs e)
+        {
+            string Path = GetFileLoc();
+            if(!Path.Contains("Settled.csv"))
+            {
+                MessageBox.Show("Please select Settled.csv");
+                txtSettled.Text = "";
+            }
+            else
+            {
+                txtSettled.Text = Path;
+            }
+        }
+
+        private void btnBrowse2_Click(object sender, EventArgs e)
+        {
+            string Path = GetFileLoc();
+            if (!Path.Contains("Unsettled.csv"))
+            {
+                MessageBox.Show("Please select Unsettled.csv");
+                txtUnsettled.Text = "";
+            }
+            else
+            {
+                txtUnsettled.Text = Path;
+            }
+        }
+
+        private void btnEvaluate_Click(object sender, EventArgs e)
+        {
+            TableSettled = FillTable(TableSettled, txtSettled.Text);
+            TableUnsettled = FillTable(TableUnsettled, txtUnsettled.Text);
+
+            DataView MyView = new DataView(TableSettled);
+
+            TableSettledSum = MyView.ToTable(true, "Customer");
+            TableSettledSum.Columns.Add("Win Rate");
+
+            int x,y;
+
+            for(x = 0; x <= TableSettledSum.Rows.Count -1; x++)
+            {
+                decimal BetCount = 0, WinCount = 0;
+                for(y=0; y <= TableSettled.Rows.Count -1; y++)
+                {
+                    if(TableSettledSum.Rows[x][0].ToString() == TableSettled.Rows[y][0].ToString())
+                    {
+                        BetCount++;
+                        if(TableSettled.Rows[y]["Win"].ToString() != "0")
+                        {
+                            WinCount++;
+                        }
+                    }
+                }
+                TableSettledSum.Rows[x]["Win Rate"] = Math.Round(((WinCount / BetCount) * 100),2);
+            }
+
+            dgvSettled.DataSource = TableSettled;
+            dgvSettledSum.DataSource = TableSettledSum;
+
+            dgvUnsettled.DataSource = TableUnsettled;
         }
     }
 }
